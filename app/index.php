2d03b7c3a6c27f2de18b0f1752f755be733b98d4
Camilo Carromeu <camilo@carromeu.com>
2016-09-16 15:21:53 -0400

    Adding a index.php to choose to see the Titan instance or public frontend in AngularJS.

---
+++ app/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<meta charset="UTF-8">
+		<title> Embaixadas Brasileiras </title>
+
+		<link rel="icon" href="manager/favicon.ico" type="image/ico" />
+		<link rel="shortcut icon" href="manager/favicon.ico" type="image/ico" />
+
+		<link rel="stylesheet" type="text/css" href="manager/index.css" />
+		<link href='http://fonts.googleapis.com/css?family=Lobster+Two' rel='stylesheet' type='text/css'>
+	</head>
+	<body>
+		<div id="main">
+			<h1><img src="manager/image/login.png" /></h1>
+			<div class="info">
+				<a href="public">Portal Público</a> (<i>frontend</i>): Em AngularJS, consumindo dados do barramento REST da instância.<br /><br />
+				<a href="manager">Gerenciador de Conteúdo</a> (<i>backend</i>): Instância do Titan que provê dados para o Portal Público por meio do barramento de serviços REST-Like padrão.
+			</div>
+		</div>
+		<div id="partners">
+			<a href="http://www.facom.ufms.br/" target="_blank"><img src="manager/image/facom.png" alt="Faculdade de Computação" /></a>
+			<a href="http://www.ufms.br/" target="_blank"><img src="manager/image/ufms.png" alt="Universidade Federal de Mato Grosso do Sul" /></a>
+			<a href="http://www.embrapa.br/gado-de-corte/" target="_blank"><img src="manager/image/embrapa.png" alt="Embrapa Gado de Corte" /></a>
+		</div>
+	</body>
+</html>
